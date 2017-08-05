f00f2b919a86eb1db1e61a1ebf3a31f955df3dc5
Eugene Valeyev <evgen.povt@gmail.com>
2017-08-05 18:08:57 +0300

    Implemented getting ben pub key from the server

--- js/wplatform-will-create.js
+++ js/wplatform-will-create.js
@@ -2,9 +2,28 @@ const Crypto = require('wcrypto');
 
 $( () => {
 
+  // Helper methods
+  function requestServer(url) {
+    const promise = $.ajax(url).done( (response) => {
+      console.log(`${url}: ${ JSON.stringify(response) }`);
+      return Promise.resolve(response);
+    }).fail( (error) => {
+      console.error(`${url}: ${ JSON.stringify(error) }`);
+      return Promise.reject(error);
+    });
+    return promise;
+  };
+
   // Button actions handlers
   $('#find-benificiary').click( (e) => {
     // request a transaction from etherscan & parse pub key
+    requestServer('http://localhost:1337/key/public').then( (response) => {
+      $('#benificiary-public-key').text(response.publicKey);
+      $('#benificiary-public-key-error').text('');
+    }).catch( (error) => {
+      $('#benificiary-public-key').text('');
+      $('#benificiary-public-key-error').text(error.statusText);
+    });
   });
 
   $('#request-key').click( (e) => {
